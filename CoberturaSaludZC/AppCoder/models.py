71dc7e4d2efeda128921a3fdc83f70860738f942
JorgeGabrielCaliz <jorgegabrielcaliz@hotmail.com>
2022-10-09 13:22:21 -0300

    Agrego el primer model

--- CoberturaSaludZC/AppCoder/models.py
+++ CoberturaSaludZC/AppCoder/models.py
@@ -1,3 +1,13 @@
+from encodings import normalize_encoding
+from logging.config import dictConfig
 from django.db import models
 
 # Create your models here.
+
+class Empadronamiento(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    dni = models.IntegerField()
+    plan = models.CharField(max_length=30)
+    email = models.EmailField()
+    
