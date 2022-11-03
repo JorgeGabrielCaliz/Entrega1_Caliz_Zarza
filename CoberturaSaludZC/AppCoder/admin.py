65341f82a0c601ddcf70d209f46bbe6be0f22870
JorgeGabrielCaliz <111112059+JorgeGabrielCaliz@users.noreply.github.com>
2022-11-03 03:28:49 -0300

    Update admin.py

--- CoberturaSaludZC/AppCoder/admin.py
+++ CoberturaSaludZC/AppCoder/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from AppCoder.models import Cartillas, Autorizaciones, Seguro_al_viajero, Plan
 
 # Register your models here.
+
+admin.site.register(Plan)
+admin.site.register(Cartillas)
+admin.site.register(Autorizaciones)
+admin.site.register(Seguro_al_viajero)
