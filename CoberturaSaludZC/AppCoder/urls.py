9ed577186d7ca3010e1ba3352ef2037e963a3c9b
JorgeGabrielCaliz <111112059+JorgeGabrielCaliz@users.noreply.github.com>
2022-11-03 03:27:33 -0300

    Update urls.py

--- CoberturaSaludZC/AppCoder/urls.py
+++ CoberturaSaludZC/AppCoder/urls.py
@@ -16,23 +16,34 @@ Including another URLconf
 from django.urls import path, include
 
 from AppCoder.views import (
+    PlanList,
+    PlanDelete,
+    PlanDetalle,
+    PlanUpdateView,
+    PlanCreacion,
     inicio,
-    empadronamiento,
-    afiliado,
     cartillas,
     autorizaciones,
     seguro_al_viajero,
+    plan,
     busqueda_2,
-    buscar_2
+    buscar_2,
 )
 
 urlpatterns = [
     path('inicio/',inicio, name="inicio"),
-    path('empadronamiento/', empadronamiento, name="empadronamiento"),
-    path('afiliado/', afiliado, name="afiliado"),
     path('cartillas/', cartillas, name="cartillas"),
     path('autorizaciones/', autorizaciones, name="autorizaciones"),
     path('seguro_al_viajero/', seguro_al_viajero, name="seguro_al_viajero"),
+    path('plan/', plan, name="plan"),
     path("busqueda_2/", busqueda_2, name="busqueda_2"),
     path("buscar_2/", buscar_2),
+    #
+    path("plan/list", PlanList.as_view(), name="Planlist"),
+    path("r'(?P<pk>\d+)^$'", PlanDetalle.as_view(), name="PlanDetail"),
+    path("plan-nuevo/", PlanCreacion.as_view(), name="PlanNew"),
+    path("r'editar/(?P<pk>\d+)^$'", PlanUpdateView.as_view(), name="PlanUpdate"),
+    
+    path("r'borrar/(?P<pk>\d+)^$'", PlanDelete.as_view(), name="PlanDelete"),
+    path('',inicio,),
 ]
